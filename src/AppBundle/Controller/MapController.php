f33696bc615966f3a5d65520d82324db6b7e2bbc
Lars Feyerabend <post@lars-feyerabend.de>
2015-10-25 21:23:23 +0100

    Place debug

--- src/AppBundle/Controller/MapController.php
+++ src/AppBundle/Controller/MapController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Person;
 use AppBundle\Form\MapFilterForm;
+use Doctrine\ORM\Query;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,4 +21,40 @@ class MapController extends Controller
             'form' => $form->createView()
         ));
     }
+
+    /**
+     * @Route("/debug-places/", name="debug_places")
+     */
+    public function debugAction(Request $request)
+    {
+        $aspects = $this->getDoctrine()->getManager()
+            ->createQuery('SELECT a, p FROM AppBundle:Aspect a LEFT JOIN a.person p WHERE a.placeName IS NOT NULL ORDER BY a.placeName')
+            ->execute();
+
+        $places = array();
+        foreach ($aspects as $aspect) {
+            $placeName = $aspect->getPlaceName();
+            if (!array_key_exists($placeName, $places)) {
+                $places[$placeName] = array(
+                    'name' => $placeName,
+                    'lat' => $aspect->getLatitude(),
+                    'lng' => $aspect->getLongitude(),
+                    'persons' => array()
+                );
+            }
+            $person = $aspect->getPerson();
+            $places[$placeName]['persons'][$person->getId()] = $person;
+        }
+        foreach ($places as &$place) {
+            $place['persons'] = "<strong>". $place['name'] ."</strong><br>".implode("<br/>", array_map(function(Person $p) {
+                return $p->getListName();
+            }, $place['persons']));
+        }
+
+        return $this->render('map/debug.html.twig', array(
+            'places' => array_values($places)
+        ));
+    }
+
+
 }
