0a7e38885862ea1224c16d589f1dd25ce4836261
Lars Feyerabend <post@lars-feyerabend.de>
2015-11-17 17:58:58 +0100

    Cryptography -> Cryptology
    According to Wikipedia, Cryptography and Cryptology are used largely
    interchangeably. If there is a slight difference in meaning,
    Cryptology would seem more appropriate, since it might include
    cryptanalysis and/or steganography.

---
+++ src/AppBundle/Entity/Place.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * Class Subject
+ * @ORM\Entity
+ * @ORM\Table(indexes={@ORM\Index(name="slug", columns={"slug"})})
+ */
+class Subject
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    /**
+     * @var Person[]|Collection
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Person", mappedBy="subjects")
+     */
+    private $associatedPersons;
+
+    /**
+     * @var Aspect[]|Collection
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Aspect", mappedBy="subjects")
+     */
+    private $associatedAspects;
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->associatedPersons = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Subject
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Add associatedPerson
+     *
+     * @param \AppBundle\Entity\Person $associatedPerson
+     *
+     * @return Subject
+     */
+    public function addAssociatedPerson(\AppBundle\Entity\Person $associatedPerson)
+    {
+        $this->associatedPersons[] = $associatedPerson;
+
+        return $this;
+    }
+
+    /**
+     * Remove associatedPerson
+     *
+     * @param \AppBundle\Entity\Person $associatedPerson
+     */
+    public function removeAssociatedPerson(\AppBundle\Entity\Person $associatedPerson)
+    {
+        $this->associatedPersons->removeElement($associatedPerson);
+    }
+
+    /**
+     * Get associatedPersons
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getAssociatedPersons()
+    {
+        return $this->associatedPersons;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Subject
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Add associatedAspect
+     *
+     * @param \AppBundle\Entity\Aspect $associatedAspect
+     *
+     * @return Subject
+     */
+    public function addAssociatedAspect(\AppBundle\Entity\Aspect $associatedAspect)
+    {
+        $this->associatedAspects[] = $associatedAspect;
+
+        return $this;
+    }
+
+    /**
+     * Remove associatedAspect
+     *
+     * @param \AppBundle\Entity\Aspect $associatedAspect
+     */
+    public function removeAssociatedAspect(\AppBundle\Entity\Aspect $associatedAspect)
+    {
+        $this->associatedAspects->removeElement($associatedAspect);
+    }
+
+    /**
+     * Get associatedAspects
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getAssociatedAspects()
+    {
+        return $this->associatedAspects;
+    }
+}
