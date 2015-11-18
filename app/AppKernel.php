71b812ecd001b948b6a7470b8cdd0f2fb37730cc
Lars Feyerabend <post@lars-feyerabend.de>
2015-11-18 09:17:21 +0100

    Include JMS Serializer
    Also minor Symfony upgrade

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
 
             new AppBundle\AppBundle(),
         );
