b9901f3832141b35a941df5c223f98564ce7ce78
Lars Feyerabend <post@lars-feyerabend.de>
2017-08-27 16:12:51 +0200

    Upgrades (mainly to symfony 3.3) and deprecation fixes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new \Symfony\Bundle\WebServerBundle\WebServerBundle(),
 
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new \Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
